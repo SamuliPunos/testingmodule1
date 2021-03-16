c00fccc6ed82127e0b9acffa2904c5a99abb9f3a
SamuliPunos <samuli.virtanen@punosmobile.com>
2021-03-16 13:10:23 +0200

    Testing module

---
+++ src/components/Test.tsx
@@ -0,0 +1,40 @@
+import React, {useEffect} from 'react';
+import {View} from 'react-native';
+import Barcode from 'react-native-barcode-builder';
+
+interface IProp {
+  cardNumber?: string;
+  holderName?: string;
+}
+
+const Test = ({cardNumber = ''}: IProp) => {
+  useEffect(() => {}, []);
+
+  return (
+    <View
+      style={{
+        transform: [{rotate: '90deg'}],
+        position: 'absolute',
+        top: '40%',
+        right: 0,
+        backgroundColor: '#ffffff',
+      }}>
+      {cardNumber !== '' ? (
+        <View style={{flex: 1}}>
+          <Barcode
+            flat
+            text={cardNumber}
+            width={5}
+            height={100}
+            value={cardNumber}
+            format={'EAN8'}
+          />
+        </View>
+      ) : (
+        <></>
+      )}
+    </View>
+  );
+};
+
+export default Test;
